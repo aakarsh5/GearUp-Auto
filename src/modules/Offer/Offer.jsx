2d21dc8e60610e3322c5b22d7b0d7f98d7d7e5a7
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-07-04 19:34:25 +0530

    Offer Letter Fixed

--- src/modules/Offer/Offer.jsx
+++ src/modules/Offer/Offer.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import'./Offer.css';
-import offer_img from '../assets/materials/offer_img1.jpg';
+import React from "react";
+import "./Offer.css";
+import offer_img from "../assets/materials/offer_img1.jpg";
 
 function Offer() {
   return (
-    <div className='offers'>
-        <div className="offer-left">
-            <h1>Exclusive</h1>
-            <h1>Offers For You</h1>
-            <p>ONLY ON PURCHASE ABOVE 999</p>
-            <button>Check Now</button>
-        </div>  
-        <div className="offer-right">
-            {/* <img src={offer_img} alt="offer image" /> */}
-        </div>
+    <div
+      className="offers"
+      style={{
+        backgroundImage: `url(${offer_img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="offer-left">
+        <h1>Exclusive</h1>
+        <h1>Offers For You</h1>
+        <p>ONLY ON PURCHASE ABOVE ₹999</p>
+        <button>Check Now</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Offer
+export default Offer;
