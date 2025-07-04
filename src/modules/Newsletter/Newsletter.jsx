8725e3e14a5c12c37be242563b1750ffebf0ad69
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-07-04 21:59:14 +0530

    Updated Newsletter

--- src/modules/Newsletter/Newsletter.jsx
+++ src/modules/Newsletter/Newsletter.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import './Newsletter.css';
+import React from "react";
+import "./Newsletter.css";
+import newsletterImg from "../assets/newsletter.jpg"; // if image is in src
 
 function Newsletter() {
   return (
-    <div className='newsletter'>
+    <div
+      className="newsletter"
+      style={{
+        backgroundImage: `url(${newsletterImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <h1>Get Updated On Gmail</h1>
       <p>Subscribe To Newsletter</p>
-      <div className='input'>
-        <input type="email" placeholder='Your Email Id' />
+      <div className="input">
+        <input type="email" placeholder="Your Email Id" />
         <button>Subscribe</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Newsletter
+export default Newsletter;
